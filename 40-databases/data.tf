af2e576fa60d256cb8a8b963a2b40b0459ad3762
lokesh <lokesh.theofficial@gmail.com>
2026-06-17 05:02:04 +0530

    actions

--- 40-databases/data.tf
+++ 40-databases/data.tf
@@ -19,7 +19,7 @@ data "aws_ami" "joindevops" {
 }
 
 data "aws_ssm_parameter" "database_subnet_ids" {
-    name = "/${var.project}/${var.environment}/public_subnet_ids"
+    name = "/${var.project}/${var.environment}/database_subnet_ids"
 }
 
 data "aws_ssm_parameter" "mongodb_sg_id" {
