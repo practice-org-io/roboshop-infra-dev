5909f95094c11c2520dd1553d0b514add8140a5d
lokesh <lokesh.theofficial@gmail.com>
2026-06-17 08:52:56 +0530

    actions

--- 40-databases/variables.tf
+++ 40-databases/variables.tf
@@ -5,3 +5,11 @@ variable "project" {
 variable "environment" {
     default = "dev"
 }
+
+variable "zone_id" {
+    default = "Z0558625374KS8URP4XXN"
+}
+
+variable "domain_name" {
+    default = "lokeshinfo.online"
+}
