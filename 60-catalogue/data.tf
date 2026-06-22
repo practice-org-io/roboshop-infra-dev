cd838e87ed4330bafad57f92c7ce6270df67565b
lokesh <lokesh.theofficial@gmail.com>
2026-06-22 15:47:29 +0530

    actions

--- 60-catalogue/data.tf
+++ 60-catalogue/data.tf
@@ -19,7 +19,7 @@ data "aws_ami" "joindevops" {
 }
 
 data "aws_ssm_parameter" "catalogue_sg_id" {
-    name = "/${var.project}/${var.environment}/backend_alb_sg_id"
+    name = "/${var.project}/${var.environment}/catalogue_sg_id"
 }
 
 data "aws_ssm_parameter" "private_subnet_ids" {
