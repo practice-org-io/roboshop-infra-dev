b6a33446aa5d34ab1c7e41f902ea0c9f19616513
lokesh <lokesh.theofficial@gmail.com>
2026-06-29 02:35:34 +0530

    actions

--- 20-sg-rules/main.tf
+++ 20-sg-rules/main.tf
@@ -291,6 +291,16 @@ resource "aws_security_group_rule" "backend_alb_frontend" {
   security_group_id = local.backend_alb_sg_id
 }
 
+resource "aws_security_group_rule" "frontend_bastion" {
+  type              = "ingress"
+  from_port         = 22
+  to_port           = 22
+  protocol          = "tcp"
+  #where traffic is coming from
+  source_security_group_id = local.bastion_sg_id
+  security_group_id = local.frontend_sg_id
+}
+
 # Frontend
 resource "aws_security_group_rule" "frontend_frontend_alb" {
   type              = "ingress"
